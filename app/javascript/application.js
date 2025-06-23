133313b01baeeef0e9e2f127087a7ff855ceaabe
jirkha <jirkha13@gmail.com>
2025-06-23 10:37:15 +0200

    Fix: resolved asset errors and updated JS config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 // This file is used to manage JavaScript for the Rails app.
 // It imports Turbo, which helps Rails handle links and forms with special methods like DELETE.
 import "@hotwired/turbo-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
+# Pin npm packages by running ./bin/importmap
 # This file is used to manage JavaScript imports for the Rails app.
 # The lines below make sure Turbo and other JavaScript modules are available in the browser.
 pin "application"
